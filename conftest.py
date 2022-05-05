c3c6b6390666b237b2b21eaa7d9a0f23c9fe6266
NelyaOs <nelya_osnova@gmail.com>
2022-05-05 21:52:25 +0700

    Change default value

--- conftest.py
+++ conftest.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="Choose browser: ru, en, ...(etc.)")
 
 
